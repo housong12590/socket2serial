fe7c4e3e8aa5d454da8457d30c6e2cf00c595e78
hous <304536797@qq.com>
2019-04-23 00:13:01 +0800

    asdfasdf

--- src/main/java/com/cin/socket2serial/Application.java
+++ src/main/java/com/cin/socket2serial/Application.java
@@ -4,6 +4,14 @@ public class Application {
 
     public static void main(String[] args) {
 
+        while (true) {
+            System.out.println("hello java");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
